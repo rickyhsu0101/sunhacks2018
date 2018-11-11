20c9b7a18e54238d0554478d163a814df344e5ad
Ricky Hsu <rickyhsu0101@gmail.com>
2018-11-11 06:50:17 -0700

    another minor fix

--- public/js/home.js
+++ public/js/home.js
@@ -1,3 +1,11 @@
 if(authObj){
   window.location.href = "/dashboard";
 }
+$("#left").on("click", function(e){
+  e.preventDefault();
+  window.location.href = "/login"
+});
+$("#right").on("click", function (e) {
+  e.preventDefault();
+  window.location.href = "/register"
+});
